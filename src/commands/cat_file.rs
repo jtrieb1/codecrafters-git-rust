d3f8effbba663e905729a4fad762c6fbe28705fc
Jacob Triebwasser <jacob.triebwasser@gmail.com>
2026-06-02 23:22:18 -0400

    codecrafters submit [skip ci]

--- src/commands/cat_file.rs
+++ src/commands/cat_file.rs
@@ -3,21 +3,21 @@ use crate::shared::object::Object;
 pub fn cat_file(flag: &str, hash: &str) {
     if flag == "p" {
         let object = Object::from_hash(hash).unwrap();
-        println!("{}", String::from_utf8_lossy(&object.content()));
+        print!("{}", String::from_utf8_lossy(&object.content()));
     } else if flag == "t" {
         let object = Object::from_hash(hash).unwrap();
-        println!("{}", object.object_type());
+        print!("{}", object.object_type());
     } else if flag == "s" {
         let object = Object::from_hash(hash).unwrap();
-        println!("{}", object.size());
+        print!("{}", object.size());
     } else if flag == "e" {
         let object_path = Object::hash_to_object_path(hash);
         if std::fs::metadata(object_path).is_ok() {
-            println!("exists");
+            print!("exists");
         } else {
-            println!("does not exist");
+            print!("does not exist");
         }
     } else {
-        println!("unknown flag: {}", flag);
+        print!("unknown flag: {}", flag);
     }
 }
